ee93f8e2e06522d18803b58ab7454709df028048
killerheman <sharmahcool5@gmail.com>
2024-12-01 17:33:41 +0530

    changes

--- app/Http/Controllers/home/HomeController.php
+++ app/Http/Controllers/home/HomeController.php
@@ -97,5 +97,19 @@ class HomeController extends Controller
         $blogs = Blog::with('blogImage')->get();
         return view('home.company.whyUs',compact('testimonial','faqs','blogs'));
     }
+    public function faq()
+    {
+        $faqs = Faq::orderBy('created_at', 'desc')->take(3)->get();
+        $testimonial=Testimonial::get();
+        $blogs = Blog::with('blogImage')->get();
+        return view('home.company.faq',compact('testimonial','faqs','blogs'));
+    }
+    public function testimonial()
+    {
+        $faqs = Faq::orderBy('created_at', 'desc')->take(3)->get();
+        $testimonial=Testimonial::get();
+        $blogs = Blog::with('blogImage')->get();
+        return view('home.company.faq',compact('testimonial','faqs','blogs'));
+    }
 
 }
