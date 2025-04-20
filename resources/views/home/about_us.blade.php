bee71a053844ca383d5951d0d127a6700b2e3db4
manish kumar <manish@zolacar.com>
2025-04-21 00:07:35 +0530

    About us page

--- resources/views/home/about_us.blade.php
+++ resources/views/home/about_us.blade.php
@@ -475,40 +475,41 @@
 
     {{-- Start Section Faq--}}
 
-    <section class="background-position-center-top sm-background-image-none"
-        style="background-image: url('{{ asset('home/assets/homepage/images/vertical-line-bg.svg') }}')">
-        <div class="container" data-aos="fade-up">
+    @if(isset($faqs) && $faqs->count()>0)
+        <section class="background-position-center-top sm-background-image-none"
+            style="background-image: url('{{ asset('home/assets/homepage/images/vertical-line-bg.svg') }}')">
+            <div class="container" data-aos="fade-up">
 
-            <div class="section-title">
-                <h2>F.A.Q</h2>
-                <h3>Frequently Asked <span>Questions</span></h3>
-            </div>
-
-            <div class="row justify-content-center">
-                <div class="col-xl-10">
-                    <ul class="faq-list">
-
-                        @foreach ($faqs as $index => $faq)
-                            <li>
-                                <div data-bs-toggle="collapse" class="collapsed question"
-                                    href="#faq{{ $index }}">
-                                    {{ $faq->question }} <i class="bi bi-chevron-down icon-show"></i><i
-                                        class="bi bi-chevron-up icon-close"></i>
-                                </div>
-                                <div id="faq{{ $index }}" class="collapse" data-bs-parent=".faq-list">
-                                    <p>{!! $faq->answer !!}</p>
-                                </div>
-                            </li>
-                        @endforeach
-
-
-
-                    </ul>
+                <div class="section-title">
+                    <h3>Frequently Asked <span>Questions</span></h3>
                 </div>
-            </div>
 
-        </div>
-    </section>
+                <div class="row justify-content-center">
+                    <div class="col-xl-10">
+                        <ul class="faq-list">
+
+                            @foreach ($faqs as $index => $faq)
+                                <li>
+                                    <div data-bs-toggle="collapse" class="collapsed question"
+                                        href="#faq{{ $index }}">
+                                        {{ $faq->question }} <i class="bi bi-chevron-down icon-show"></i><i
+                                            class="bi bi-chevron-up icon-close"></i>
+                                    </div>
+                                    <div id="faq{{ $index }}" class="collapse" data-bs-parent=".faq-list">
+                                        <p>{!! $faq->answer !!}</p>
+                                    </div>
+                                </li>
+                            @endforeach
+
+
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    @endif
     {{-- End Section Faq--}}
 
     {{-- start Blog section --}}
