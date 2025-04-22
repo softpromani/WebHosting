0fbd2c92ea323f1bb62cfbb7532da30f6c8f055f
manish kumar <manish@zolacar.com>
2025-04-22 12:11:46 +0530

    pdp page

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -1605,7 +1605,7 @@
                 </p>
                 <div class="d-flex align-items-center">
                   <div class="me-3 fs-3">
-                    <i class="fa-thin fa-circle-phone text-white"></i>
+                    <i class="fa-solid fa-circle-phone bg-white"></i>
                   </div>
                   <div>
                     <div class="fw-bold">Call Us: 258-3258 235</div>
