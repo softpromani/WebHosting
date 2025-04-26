41fdb5233c132ee45efcdbf1d2bad752e5bd42c0
manish kumar <manish@zolacar.com>
2025-04-26 15:00:48 +0530

    partner page arrange section

--- resources/views/home/company/partner.blade.php
+++ resources/views/home/company/partner.blade.php
@@ -410,9 +410,35 @@
                 </div>
             </div>
         </section>
+        <!-- ======= Testimonials Section ======= -->
+        <section id="testimonials" class="testimonials">
+            <div class="container" data-aos="zoom-in">
+
+                <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+                    <div class="swiper-wrapper">
+                        @foreach ($testimonial as $t)
+                            <div class="swiper-slide">
+                                <div class="testimonial-item">
+                                    <img src="{{ asset($t->pic) }}" class="testimonial-img" alt="">
+                                    <h3>{{ $t->name }}</h3>
+                                    <h4>{{ $t->position }}</h4>
+                                    <p>
+                                        <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                        {{ $t->description }}
+                                        <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                                    </p>
+                                </div>
+                            </div><!-- End testimonial item -->
+                        @endforeach
 
 
+                    </div>
+                    <div class="swiper-pagination"></div>
+                </div>
 
+            </div>
+        </section><!-- End Testimonials Section -->
+        <!-- ======= F.A.Q Section ======= -->
         <section id="faq" class="faq section-bg">
             <div class="container" data-aos="fade-up">
 
@@ -449,80 +475,11 @@
 
             </div>
         </section>
-
-
-
-        <!-- ======= Testimonials Section ======= -->
-        <section id="testimonials" class="testimonials">
-            <div class="container" data-aos="zoom-in">
-
-                <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
-                    <div class="swiper-wrapper">
-                        @foreach ($testimonial as $t)
-                            <div class="swiper-slide">
-                                <div class="testimonial-item">
-                                    <img src="{{ asset($t->pic) }}" class="testimonial-img" alt="">
-                                    <h3>{{ $t->name }}</h3>
-                                    <h4>{{ $t->position }}</h4>
-                                    <p>
-                                        <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                        {{ $t->description }}
-                                        <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                                    </p>
-                                </div>
-                            </div><!-- End testimonial item -->
-                        @endforeach
-
-
-                    </div>
-                    <div class="swiper-pagination"></div>
-                </div>
-
-            </div>
-        </section><!-- End Testimonials Section -->
-
-
+        {{-- Blog section --}}
         <section class="user-blog" class="footer-top">
-            <div class="container">
-
-                <div class="heading">
-                    Blogs
-                </div>
-
-                <div class="swiper blogSwiper mb-5">
-                    <div class="swiper-wrapper p-3">
-                        @foreach ($blogs as $blog)
-                            <div class="swiper-slide ">
-                                <div class="card">
-                                    <img src="{{ asset('storage/' . $blog->blogImage->media) }}"
-                                        class="card-img-top blogCard" alt="{{ $blog->title }}">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{ $blog->title }}</h5>
-                                        <p class="card-text">{!! Str::limit(strip_tags($blog->description), 100) !!}</p>
-                                        <button type="button" class="btn btn-primary" data-toggle="modal"
-                                            data-target="#blogModal" data-title="{{ $blog->title }}"
-                                            data-description="{{ $blog->description }}">
-                                            Read more...
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-
-                    <div class="swiper-pagination"></div>
-                    <div class="swiper-button-next"></div>
-                    <div class="swiper-button-prev"></div>
-                </div>
-            </div>
-
-
-
+            <x-blog :blogs="$blogs" />
         </section>
-
-
-
-
+        {{-- End Blog --}}
 
     </main><!-- End #main -->
 
