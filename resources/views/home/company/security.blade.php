ea4a46c296d49b5d282f5ac2eb836554d1f26437
manish kumar <manish@zolacar.com>
2025-04-26 22:13:55 +0530

    about-us page ui blog

--- resources/views/home/company/security.blade.php
+++ resources/views/home/company/security.blade.php
@@ -825,44 +825,7 @@
         </section>
 
         <section class="user-blog" class="footer-top">
-            <div class="container">
-
-                <div class="heading">
-                    Blogs
-                </div>
-
-
-                <div class="swiper blogSwiper mb-5">
-                    <div class="swiper-wrapper p-3">
-                        @forelse ($blogs as $blog)
-                            <div class="swiper-slide ">
-                                <div class="card">
-                                    <img src="{{ asset('storage/' . $blog->blogImage->media) }}"
-                                        class="card-img-top blogCard" alt="{{ $blog->title }}">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{ $blog->title }}</h5>
-                                        <p class="card-text">{!! Str::limit(strip_tags($blog->description), 100) !!}</p>
-                                        <button type="button" class="btn btn-primary" data-toggle="modal"
-                                            data-target="#blogModal" data-title="{{ $blog->title }}"
-                                            data-description="{{ $blog->description }}">
-                                            Read more...
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        @empty
-                            <p>No Blog Found!</p>
-                        @endforelse
-                    </div>
-
-                    <div class="swiper-pagination"></div>
-                    <div class="swiper-button-next"></div>
-                    <div class="swiper-button-prev"></div>
-                </div>
-            </div>
-
-
-
+            <x-blog-grid :blogs="$blogs" page="Our Security" />
         </section>
 
 
