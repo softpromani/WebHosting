25859ee1c1a5f1345c50c3ffb95367adf3011023
manish kumar <manish@zolacar.com>
2025-04-29 17:32:37 +0530

    pdp page cta btn

--- resources/views/components/cta-btn.blade.php
+++ resources/views/components/cta-btn.blade.php
@@ -2,9 +2,7 @@
     <div class="row d-flex justify-content-evenly">
         @foreach ($links as $key=>$link)  
             <div class="col-lg-3 col-md-4 col-6">
-                <div class="cta-btn">
-                    <a href="{{ url($link) }}" class="btn btn-primary w-100">{{ucfirst(str_replace('_',' ',$key))}}</a>
-                </div>
+                    <a href="{{ url($link) }}" class="btn btn-primary">{{ucfirst(str_replace('_',' ',$key))}}</a>
             </div>
         @endforeach
     </div>
