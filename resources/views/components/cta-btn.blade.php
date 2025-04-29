4d15fe88dc6331ef9de4e564cd7a29c326eb9c3b
manish kumar <manish@zolacar.com>
2025-04-29 17:15:26 +0530

    pdp page cta

--- resources/views/components/cta-btn.blade.php
+++ resources/views/components/cta-btn.blade.php
@@ -1,12 +1,11 @@
 <div>
     <div class="row d-flex justify-content-evenly">
-        @forelse ($links as $key=>$link)  
+        @foreach ($links as $key=>$link)  
             <div class="col-lg-3 col-md-4 col-6">
                 <div class="cta-btn">
                     <a href="{{ url($link) }}" class="btn btn-parimary">{{(str_replace('_',' ',$key))}}</a>
                 </div>
             </div>
-        @empty
-        @endforelse
+        @endforeach
     </div>
 </div>
