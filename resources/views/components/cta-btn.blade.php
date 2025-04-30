46e42899355f3a24603be187232abde5994d27c0
manish kumar <manish@zolacar.com>
2025-04-30 13:25:02 +0530

    pdp business page dynamic

--- resources/views/components/cta-btn.blade.php
+++ resources/views/components/cta-btn.blade.php
@@ -1,7 +1,7 @@
 <div class="w-100">
     <div class="row d-flex justify-content-evenly">
         @foreach ($links as $key=>$link)  
-            <div class="col-lg-6">
+            <div class="col-6 mb-2">
                     <a href="{{ url($link) }}" class="btn btn-primary">{{ucfirst(str_replace('_',' ',$key))}}</a>
             </div>
         @endforeach
