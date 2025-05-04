8b2970152131ed7a27914a645a0668575f5b0cc2
manish kumar <manish@zolacar.com>
2025-05-04 21:01:29 +0530

    home cta btn

--- resources/views/components/cta-btn.blade.php
+++ resources/views/components/cta-btn.blade.php
@@ -1,8 +1,14 @@
 <div class="w-100">
     <div class="row d-flex justify-content-evenly">
+        @php
+            $colors = ['primary', 'success', 'info', 'warning', 'secondary', 'danger', 'dark', 'light'];
+        @endphp
         <div class="col-md-12 d-flex gap-2">
-            @foreach ($links as $key=>$link) 
-                <a href="{{ url($link) }}" class="btn btn-primary">{{ucfirst(str_replace('_',' ',$key))}}</a>
+            @foreach ($links as $key=>$link)
+                @php
+                    $colorClass = $colors[$loop->index % count($colors)];
+                @endphp 
+                <a href="{{ url($link) }}" class="btn btn-{{ $colorClass }}">{{ucfirst(str_replace('_',' ',$key))}}</a>
             @endforeach
         </div>
     </div>
