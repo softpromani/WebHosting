74a15d5ce9de3e357ca5625499ff3c8774bc19ea
manish kumar <manish@zolacar.com>
2025-05-04 20:55:11 +0530

    home cta btn

--- resources/views/components/cta-btn.blade.php
+++ resources/views/components/cta-btn.blade.php
@@ -1,9 +1,9 @@
 <div class="w-100">
     <div class="row d-flex justify-content-evenly">
-        @foreach ($links as $key=>$link)  
-            <div class="col-6 mb-2">
-                    <a href="{{ url($link) }}" class="btn btn-primary">{{ucfirst(str_replace('_',' ',$key))}}</a>
-            </div>
-        @endforeach
+        <div class="col-md-12 d-flex gap-2">
+            @foreach ($links as $key=>$link) 
+                <a href="{{ url($link) }}" class="btn btn-primary">{{ucfirst(str_replace('_',' ',$key))}}</a>
+            @endforeach
+        </div>
     </div>
 </div>
