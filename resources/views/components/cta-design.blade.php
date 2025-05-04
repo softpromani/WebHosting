e28be83eec8d355a8202213ffd1140883d6a414d
manish kumar <manish@zolacar.com>
2025-05-04 21:17:41 +0530

    home cta design

---
+++ resources/views/components/cta-design.blade.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 w-100">
+
+        </div>
+        <div class="col-md-4 w-100" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
+        </div>
+        <div class="col-md-4 w-100" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
+
+        </div>
+        <div class="col-md-4 w-100 bg-light">
+            
+        </div>
+        <div class="col-md-4 w-100" style="background: var(--secondary-color)" >
+            
+        </div>
+    </div>
+</div>
