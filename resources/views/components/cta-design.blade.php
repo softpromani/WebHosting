2ccee8dedc7ceaac6acec487ffedb23b7991c95e
manish kumar <manish@zolacar.com>
2025-05-04 21:25:53 +0530

    home cta design

--- resources/views/components/cta-design.blade.php
+++ resources/views/components/cta-design.blade.php
@@ -1,34 +1,34 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-8 mb-3">
+        <div class="col-md-8 mb-5">
             <div class="card shadow">
                 <div class="card-body rounded">
 
                 </div>
             </div>
         </div>
-        <div class="col-md-4 mb-3">
+        <div class="col-md-4 mb-5">
             <div class="card shadow" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
                 <div class="card-body rounded">
                     
                 </div>
             </div>
         </div>
-        <div class="col-md-4 mb-3">
+        <div class="col-md-4 mb-5">
             <div class="card shadow" >
                 <div class="card-body rounded" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
                     
                 </div>
             </div>
         </div>
-        <div class="col-md-4 mb-3 ">
+        <div class="col-md-4 mb-5 ">
             <div class="card bg-light">
                 <div class="card-body rounded">
                     
                 </div>
             </div>
         </div>
-        <div class="col-md-4 mb-3" >
+        <div class="col-md-4 mb-5" >
             <div class="card shadow">
                 <div class="card-body rounded" style="background: var(--secondary-color)" >
                     
