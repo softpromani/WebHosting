75eee19437d94e05a26cc84d556bead078464e76
manish kumar <manish@zolacar.com>
2025-05-04 21:50:32 +0530

    home cta design

--- resources/views/components/cta-design.blade.php
+++ resources/views/components/cta-design.blade.php
@@ -15,28 +15,41 @@
         <div class="col-md-4 mb-5">
             <div class="card shadow rounded-4" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
                 <div class="card-body">
-                    
+                    <h2>Fast & Secure Access</h2>
+                    <p>
+                        Enjoy seamless access to your QuickBooks Pro anytime,anywhere.
+                    </p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-5">
             <div class="card shadow rounded-4" >
                 <div class="card-body rounded-4" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
-                    
+                    <h2>Reliable Support</h2>
+                    <p>
+                        Count on our expert team for assistance when you need it.
+                    </p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-5 ">
             <div class="card shadow bg-light rounded-4">
                 <div class="card-body">
-                    
+                    <h2>Scalable Solutions</h2>
+                    <p>
+                        Trailored hosting plans that grow with your business.
+                    </p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-5" >
             <div class="card shadow rounded-4">
                 <div class="card-body rounded-4" style="background: var(--secondary-color)" >
-                    
+                    <h2>Enhanced Collaboration</h2>
+                    <p>
+                        Work effortlessly with your team in real-time.
+                            
+                    </p>
                 </div>
             </div>
         </div>
