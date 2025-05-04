87618c79e46a427a3e8ff65f9ded40530ef540fc
manish kumar <manish@zolacar.com>
2025-05-04 21:30:33 +0530

    home cta design

--- resources/views/components/cta-design.blade.php
+++ resources/views/components/cta-design.blade.php
@@ -6,36 +6,36 @@
     </style>
     <div class="row">
         <div class="col-md-8 mb-5">
-            <div class="card shadow">
-                <div class="card-body rounded-4">
+            <div class="card shadow rounded-4">
+                <div class="card-body">
 
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-5">
-            <div class="card shadow" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
-                <div class="card-body rounded-4">
+            <div class="card shadow rounded-4" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
+                <div class="card-body">
                     
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-5">
-            <div class="card shadow" >
-                <div class="card-body rounded-4" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
+            <div class="card shadow rounded-4" >
+                <div class="card-body" style="background: linear-gradient(to right,var(--primary-color), #e9ecef);" >
                     
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-5 ">
-            <div class="card bg-light">
-                <div class="card-body rounded-4">
+            <div class="card bg-light rounded-4">
+                <div class="card-body">
                     
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-5" >
-            <div class="card shadow">
-                <div class="card-body rounded-4" style="background: var(--secondary-color)" >
+            <div class="card shadow rounded-4">
+                <div class="card-body" style="background: var(--secondary-color)" >
                     
                 </div>
             </div>
