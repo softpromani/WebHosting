1d52c7c9843f22cc333b3d79b04fb2f940079adf
manish kumar <manish@zolacar.com>
2025-05-09 11:35:40 +0530

    slider

--- resources/views/components/banner-slider.blade.php
+++ resources/views/components/banner-slider.blade.php
@@ -14,7 +14,7 @@
            border-radius: 0px 30px 30px 0px;
         }
     </style>
-    <div id="carouselExampleCaptions" class="carousel slide carousel-dark" data-bs-ride="carousel" style="top:1.2rem; !important;">
+    <div id="carouselExampleCaptions" class="carousel slide carousel-dark" data-bs-ride="carousel">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -22,7 +22,7 @@
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img src="{{ asset('home/banner-slider/Home header-slider1.webp') }}" class="d-block w-100" alt="Slider-1">
-            <div class="carousel-caption">
+            <div class="carousel-caption" style="top:1.2rem; !important;">
               <h5>Streamline Your Business Operations with Secured Cloud Hosting Services</h5>
               <p>Power your business with premium cloud hosting solutions by Mounteko. With lightning-fast performance, 99.95% uptime, and expert support, we provide the perfect cloud hosting platform for your growing business.</p>
             </div>
