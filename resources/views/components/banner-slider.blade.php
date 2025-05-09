df5984de7f043a9fe3f634394a81658e7a3c04d9
manish kumar <manish@zolacar.com>
2025-05-09 12:32:19 +0530

    slider

--- resources/views/components/banner-slider.blade.php
+++ resources/views/components/banner-slider.blade.php
@@ -52,28 +52,24 @@
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
                 aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
+            {{-- <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                aria-label="Slide 2"></button> --}}
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="{{ asset('home/banner-slider/Home header-slider1.webp') }}" class="d-block w-100"
+                <img src="{{ asset('home/banner-slider/slider-1.webp') }}" class="d-block w-100"
                     alt="Slider-1">
                   <div class="carousel-caption d-flex flex-column justify-content-center align-items-start h-100 text-start">
-                      <h5 class="caption-title">Streamline Your Business Operations with Secured Cloud Hosting Services</h5>
-                      <p class="caption-text">Power your business with premium cloud hosting solutions by Mounteko. With lightning-fast
-                          performance, 99.95% uptime, and expert support, we provide the perfect cloud hosting platform
-                          for your growing business.</p>
                   </div>
             </div>
-            <div class="carousel-item">
+            {{-- <div class="carousel-item">
                 <img src="{{ asset('home/banner-slider/slider-1.webp') }}" class="d-block w-100"
                     alt="slider-2">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Second slide label</h5>
                     <p>Some representative placeholder content for the second slide.</p>
                 </div>
-            </div>
+            </div> --}}
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
             data-bs-slide="prev">
