f464f19c6e684d182a5f189c5f62ae7152d30e10
manish kumar <manish@zolacar.com>
2025-05-09 11:17:59 +0530

    slider

--- resources/views/components/banner-slider.blade.php
+++ resources/views/components/banner-slider.blade.php
@@ -21,14 +21,14 @@
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="{{ asset('home/banner-slider/Home header-slider1.webp') }}" class="d-block w-100" alt="{{ asset('home/banner-slider/img-slider/images/slider(1).webp') }}">
-            <div class="carousel-caption d-none d-md-block">
+            <img src="{{ asset('home/banner-slider/Home header-slider1.webp') }}" class="d-block w-100" alt="Slider-1">
+            <div class="carousel-caption">
               <h5>First slide label</h5>
               <p>Some representative placeholder content for the first slide.</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="{{ asset('home/banner-slider/img-slider/images/slider(2).webp') }}" class="d-block w-100" alt="{{ asset('home/banner-slider/img-slider/images/slider(1).webp') }}">
+            <img src="{{ asset('home/banner-slider/img-slider/images/slider(2).webp') }}" class="d-block w-100" alt="slider-2">
             <div class="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
               <p>Some representative placeholder content for the second slide.</p>
