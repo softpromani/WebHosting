da257368c3e79ab88d06ac4c20f7f28b0fee84b0
manish kumar <manish@zolacar.com>
2025-05-09 11:34:37 +0530

    slider

--- resources/views/components/banner-slider.blade.php
+++ resources/views/components/banner-slider.blade.php
@@ -14,7 +14,7 @@
            border-radius: 0px 30px 30px 0px;
         }
     </style>
-    <div id="carouselExampleCaptions" class="carousel slide carousel-dark" data-bs-ride="carousel" style="top:1.2rem;">
+    <div id="carouselExampleCaptions" class="carousel slide carousel-dark" data-bs-ride="carousel" style="top:1.2rem; !important;">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
